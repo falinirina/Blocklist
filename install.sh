be86560caa3760297a56b67015faa67f557ad1fc
falinirina <85443405+falinirina@users.noreply.github.com>
2023-10-30 16:15:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
-mkfs.fat -F32 -n BOOT /dev/sda1
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
-curl https://raw.githubusercontent.com/falinirina/Blocklist/main/mirror.txt > /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel linux linux-firmware
-genfstab -p /mnt >> /mnt/etc/fstab
+pacman-key --init
+pacman-key --populate archlinux
+pacman -S neovim man-db man-pages texinfo elinks nano
+ln -sf /usr/share/zoneinfo/Indian/Antananarivo /etc/localtime
+hwclock --systohc
+echo "landry" > /etc/hostname
+echo "# IPv4 Hosts
+127.0.0.1	localhost myhostname
+
+# Machine FQDN
+127.0.1.1	myhostname.localdomain	myhostname
+
+# IPv6 Hosts
+::1		localhost	ip6-localhost	ip6-loopback
+ff02::1 	ip6-allnodes
+ff02::2 	ip6-allrouters" > /etc/hosts
+mkinitcpio -p linux
+bootctl --path=/boot install
