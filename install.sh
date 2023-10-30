b1f4e52b98089755d9d10445fabcc1da7a39e7be
falinirina <85443405+falinirina@users.noreply.github.com>
2023-10-30 16:39:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
-pacman-key --init
-pacman-key --populate archlinux
-pacman -S neovim man-db man-pages texinfo elinks nano
-ln -sf /usr/share/zoneinfo/Indian/Antananarivo /etc/localtime
-hwclock --systohc
-echo "landry" > /etc/hostname
-echo "# IPv4 Hosts
-127.0.0.1	localhost myhostname
-
-# Machine FQDN
-127.0.1.1	myhostname.localdomain	myhostname
-
-# IPv6 Hosts
-::1		localhost	ip6-localhost	ip6-loopback
-ff02::1 	ip6-allnodes
-ff02::2 	ip6-allrouters" > /etc/hosts
-mkinitcpio -p linux
-bootctl --path=/boot install
+echo "title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+options root=UUID=7bc17951-50af-4232-b078-d99990fcf78 rw" >> /boot/loader/entries/arch.conf
